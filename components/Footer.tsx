ff165a0169cb20803f0b0bc0b97f7b564277fa88
YinML1205 <ngaoan64@gmail.com>
2026-03-17 09:03:07 +0700

    fixing footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-100 px-16 py-16">
+    <footer className="bg-gray-300 px-16 py-16">
 
       <div className="grid grid-cols-4 gap-10">
 
         <div>
-          <h4 className="font-semibold mb-4">Khám phá</h4>
-          <ul className="space-y-2 text-sm">
+          <h4 className="font-semibold mb-4  text-black ">Khám phá</h4>
+          <ul className="space-y-2 text-sm  text-gray-500">
             <li>Charms</li>
             <li>Vòng tay</li>
             <li>Dây chuyền</li>
@@ -16,11 +16,11 @@ export default function Footer() {
         </div>
 
         <div>
-          <h4 className="font-semibold mb-4">
+          <h4 className="font-semibold mb-4  text-black ">
             Chăm sóc khách hàng
           </h4>
 
-          <ul className="space-y-2 text-sm">
+          <ul className="space-y-2 text-sm  text-gray-500">
             <li>Câu hỏi thường gặp</li>
             <li>Chính sách bảo mật</li>
             <li>Chính sách thanh toán</li>
@@ -28,11 +28,11 @@ export default function Footer() {
         </div>
 
         <div>
-          <h4 className="font-semibold mb-4">
+          <h4 className="font-semibold mb-4  text-black ">
             Dịch vụ giao hàng
           </h4>
 
-          <ul className="space-y-2 text-sm">
+          <ul className="space-y-2 text-sm  text-gray-500">
             <li>Giao hàng tiết kiệm</li>
             <li>Ahamove</li>
             <li>Kiểm tra đơn hàng</li>
@@ -40,11 +40,11 @@ export default function Footer() {
         </div>
 
         <div>
-          <h4 className="font-semibold mb-4">
+          <h4 className="font-semibold mb-4  text-black ">
             Về chúng tôi
           </h4>
 
-          <ul className="space-y-2 text-sm">
+          <ul className="space-y-2 text-sm  text-gray-500">
             <li>Về Pandora</li>
             <li>Tuyển dụng</li>
             <li>Blogs</li>
@@ -53,7 +53,7 @@ export default function Footer() {
 
       </div>
 
-      <p className="text-center text-sm mt-10">
+      <p className="text-center text-sm mt-10  text-gray-700">
         © Pandora Jewelry, LLC. All rights reserved.
       </p>
 
